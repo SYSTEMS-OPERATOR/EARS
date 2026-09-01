e115f351959cf8801160261869e04580695ae195
SYSTEMS-OPERATOR <155610697+SYSTEMS-OPERATOR@users.noreply.github.com>
2026-09-01 19:30:26 -0400

    Apply lint fixes to src/ears/__init__.py

--- src/ears/__init__.py
+++ src/ears/__init__.py
@@ -1,9 +1,9 @@
 """Auditory hardware interfaces for the SOPHY EARS project."""
 
 from .xvf3800 import (
+    XVF3800,
     DeviceNotFoundError,
     DirectionOfArrival,
-    XVF3800,
     XVF3800Error,
 )
 
